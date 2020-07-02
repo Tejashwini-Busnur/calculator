34433efe9e1d4b0a6eeaae8bc1a11f1f4937d2f0
Tejashwini-Busnur <tejashwini.b.19@gmail.com>
2020-07-01 23:02:02 -0400

    changes for heroku build

--- src/App.js
+++ src/App.js
@@ -1,84 +1,81 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import DisplayComponent from './components/DisplayComponent';
-import KeyPadComponent from "./components/KeyPadComponent";
+import KeyPadComponent from "./components/KeyPadComponent"
 
-class App extends Component {
-    constructor(){
-        super();
 
-        this.state = {
-            display: ""
-        }
-    }
 
-    onClick = button => {
+    class App extends Component {
 
-        if(button === "="){
-            this.calculate()
+
+        constructor() {
+            super();
+
+            this.state = {
+                display: ""
+            }
         }
 
-        else if(button === "C"){
-            this.reset()
-        }
-        else if(button === "CE"){
-            this.backspace()
-        }
+        onClick = button => {
 
-        else {
+            if (button === "=") {
+                this.calculate()
+            } else if (button === "C") {
+                this.reset()
+            } else if (button === "CE") {
+                this.backspace()
+            } else {
+                this.setState({
+                    display: this.state.display + button
+                })
+            }
+        };
+
+
+        calculate = () => {
+            var checkDisplay = ''
+            if (this.state.display.includes('--')) {
+                checkDisplay = this.state.display.replace('--', '+')
+            } else {
+                checkDisplay = this.state.display
+            }
+
+            try {
+                this.setState({
+                    // eslint-disable-next-line
+                    display: (eval(checkDisplay) || "") + ""
+                })
+            } catch (e) {
+                this.setState({
+                    display: "error"
+                })
+
+            }
+        };
+
+        reset = () => {
             this.setState({
-                display: this.state.display + button
+                display: ""
             })
-        }
-    };
+        };
 
-
-    calculate = () => {
-        var checkDisplay = ''
-        if(this.state.display.includes('--')){
-            checkDisplay = this.state.display.replace('--','+')
-        }
-
-        else {
-            checkDisplay = this.state.display
-        }
-
-        try {
+        backspace = () => {
             this.setState({
-                // eslint-disable-next-line
-                display: (eval(checkDisplay) || "" ) + ""
+                display: this.state.display.slice(0, -1)
             })
-        } catch (e) {
-            this.setState({
-                display: "error"
-            })
+        };
 
-        }
-    };
-
-    reset = () => {
-        this.setState({
-            display: ""
-        })
-    };
-
-    backspace = () => {
-        this.setState({
-            display: this.state.display.slice(0, -1)
-        })
-    };
-
-    render() {
-        return (
-            <div>
-                <div className="calculator-body">
-                    <h1 class = "h1" >Simple Calculator</h1>
-                    <DisplayComponent display={this.state.display}/>
-                    <KeyPadComponent onClick={this.onClick}/>
+        render() {
+            return (
+                <div>
+                    <div className="calculator-body">
+                        <h1 class="h1">Simple Calculator</h1>
+                        <DisplayComponent display={this.state.display}/>
+                        <KeyPadComponent onClick={this.onClick}/>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
-}
 
 export default App;
